6b4f3b4d32a082db35cdb5e383449341c68e42ae
Maaloum <50062179+maaloum@users.noreply.github.com>
2023-03-31 02:11:29 +0300

    rename UserId to AutherId

--- db/migrate/20230330230714_rename_user_idto_auther_id.rb
+++ db/migrate/20230330230714_rename_user_idto_auther_id.rb
@@ -1,5 +1,5 @@
 class RenameUserIdtoAutherId < ActiveRecord::Migration[7.0]
   def change
-    rename_column :posts, :user_id, :auther_id
+    rename_column :posts, :users_id, :auther_id
   end
 end

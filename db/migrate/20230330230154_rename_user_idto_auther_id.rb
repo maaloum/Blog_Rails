6274a08cbb7d759dcd1fe9e87ff4302d64b27cb6
Maaloum <50062179+maaloum@users.noreply.github.com>
2023-03-31 02:05:14 +0300

    create association between User and Post

--- db/migrate/20230330230154_rename_user_idto_auther_id.rb
+++
@@ -1,5 +0,0 @@
-class RenameUserIdtoAutherId < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :posts, :user_id, :auther_id
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_30_225646) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_30_230053) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_30_225646) do
     t.integer "likescounter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "users_id", null: false
+    t.index ["users_id"], name: "index_posts_on_users_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -55,4 +57,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_30_225646) do
   add_foreign_key "comments", "users", column: "users_id"
   add_foreign_key "likes", "posts", column: "posts_id"
   add_foreign_key "likes", "users", column: "users_id"
+  add_foreign_key "posts", "users", column: "users_id"
 end

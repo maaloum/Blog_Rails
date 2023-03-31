b6dcadce41186d934cb9f932f27adfa43c315d3a
Maaloum <50062179+maaloum@users.noreply.github.com>
2023-03-31 04:58:29 +0300

    Create Comment method

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,12 @@
 class Comment < ApplicationRecord
   belongs_to :user
+  after_save :comment_counter
+
+  after_save :update_comments_counter
+
+  private
+
+  def comment_counter
+    post.increment!(:comments_counter)
+  end
 end

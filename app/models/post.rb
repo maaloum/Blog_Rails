d47cd60e75f89e0dc0e66a813540422f603c79ae
Maaloum <50062179+maaloum@users.noreply.github.com>
2023-03-31 04:57:36 +0300

    Create post methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,16 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :comments
   belongs_to :user
+
+  def latest_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  after_save :update_posts_counter
+
+  private
+
+  def post_counter
+    author.increment!(:posts_counter)
+  end
 end

71a5102951da9cc169f1d70a37e466bbc93b95b8
Maaloum <50062179+maaloum@users.noreply.github.com>
2023-04-01 11:51:45 +0300

    add validations for models

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,9 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :comments
   belongs_to :user
+  validates :title, presence: true, length: { in: 1..250 }
+  validates :comments_counter, :likes_counter, comparison: { greater_than_or_equal_to: 0 },
+                                               numericality: { only_integer: true }
 
   def latest_comments
     comments.order(created_at: :desc).limit(5)
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :posts
   has_many :likes
   has_many :comments
+  validates :name, presence: true
+  validates :post_counter, comparison: { greater_than_or_equal_to: 0 }, numericality: { only_integer: true }
 
   def three_most_recent_posts
     posts.order(created_at: :desc).limit(3)

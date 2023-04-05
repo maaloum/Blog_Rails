e10f79a719f5818691299bc8ffd3b24c12311311
Maaloum <50062179+maaloum@users.noreply.github.com>
2023-04-06 01:52:35 +0300

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
     # resources :comments
     root "users#index"
     resources :users, only: [:index, :show] do 
-        resources :posts, only: [:index, :show, :new, :create]
+        resources :posts, only: [:index, :show, :new, :create] do 
+            resources :comments, only: [:new, :create]
+            resources :likes, only: [:create]
+        end
     end
 
 end

a59ae21b8226bae9dfab7b76cee188e1cd672e98
Maaloum <50062179+maaloum@users.noreply.github.com>
2023-04-14 14:45:16 +0300

    style the logins pages

--- db/migrate/20230413140233_add_devise_to_users.rb
+++ db/migrate/20230413140233_add_devise_to_users.rb
@@ -1,7 +1,7 @@
 
-class DeviseCreateUsers < ActiveRecord::Migration[6.0]
+class AddDeviseToUser < ActiveRecord::Migration[6.0]
   def change
-    create_table :users do |t|
+    change_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
